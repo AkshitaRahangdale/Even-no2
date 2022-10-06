547c66a9d349ea1e73ce367974add4a4faf256ff
AkshitaRahangdale <akshitarahangdale@1720gmail.com>
2022-10-06 12:53:17 +0530

    Add project files.

---
+++ Even no2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_no2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 5) 
+            {
+                Console.WriteLine("hello");
+                i++; ;
+            }
+        }
+    }
+}
